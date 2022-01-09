f8bbc382658298b3b48cde402ba607db476ab4f1
Prasanna Anbazhagan <praslnx8@gmail.com>
2022-01-09 22:29:10 +0530

    feat: Wire up image upload api by converting base64 to file.

--- src/components/ProductImageCapture.tsx
+++ src/components/ProductImageCapture.tsx
@@ -19,20 +19,38 @@ const videoConstraints = {
     const capture = React.useCallback(
       () => {
         const imageSrc = webcamRef.current.getScreenshot();
+        const file = dataURLtoFile(imageSrc, 'image.jpg');
+        const data = new FormData();
+        data.append('file', file);
         const config = {
             headers: { 'content-type': 'multipart/form-data' }
-           }
+        }
 
-        axios.post('http://127.0.0.1:4000/image-search', imageSrc,config)
-            .then(res => {
-                console.log("image detected", res.data);
-                returnImageScanResult(res.data);
-                
+        axios.post('http://127.0.0.1:4000/image-search', data, config).then(res => {
+                if(res.data.length > 0) {
+                    returnImageScanResult(res.data[0].className);
+                } else {
+                    returnImageScanResult('');
+                }
             })
         .catch(err => console.log(err));
-      },
-      [webcamRef]
+      }, [webcamRef]
     );
+
+    function dataURLtoFile(dataurl:any, filename:string) {
+
+        let arr = dataurl.split(','),
+            mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]),
+            n = bstr.length,
+            u8arr = new Uint8Array(n);
+
+        while(n--){
+           u8arr[n] = bstr.charCodeAt(n);
+       }
+
+       return new File([u8arr], filename, {type:mime});
+    }
   
     return (
       <>
